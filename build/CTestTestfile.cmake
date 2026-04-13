a4dc89a9ca40df363fd14b9593affac24d6e7173
6D1 <haoxuanliang9@gmail.com>
2026-04-13 19:33:15 +0800

    add DNSCodec

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/Administrator/Desktop/Filter
+# Build directory: /mnt/c/Users/Administrator/Desktop/Filter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(DNSCodecTest "/mnt/c/Users/Administrator/Desktop/Filter/build/test_dns_codec")
+set_tests_properties(DNSCodecTest PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/Administrator/Desktop/Filter/CMakeLists.txt;36;add_test;/mnt/c/Users/Administrator/Desktop/Filter/CMakeLists.txt;0;")
